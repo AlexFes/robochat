8b1c2c6db87dddba55a0b10d0f9a0bafdab43c81
Alex Fes <alexeiruxur@gmail.com>
2019-08-14 14:32:44 +0300

    auth flow

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,27 @@ var router = express.Router();
 const util = require('util');
 const config = require('../config');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+let access_token = '';
+let user_id = '';
+
+// Get access token
+router.get('/', function(req, res) {
+    res.redirect('https://oauth.vk.com/authorize?client_id=' + config.clientId +
+        '&redirect_uri=' + config.host +
+        'code&scope=messages,offline&response_type=code&v=5.92');
+});
+
+router.get('/code', (req, res) => {
+    res.redirect('https://oauth.vk.com/access_token?client_id=' + config.clientId +
+        '&redirect_uri=' + config.host +
+        'auth&code=' + req.query.code + '&client_secret=' + config.clientSecret);
+});
+
+router.post('/auth', (req, res) => {
+    access_token = req.body.access_token;
+    user_id = req.body.user_id;
+    console.log("Access_token: " + access_token + "\nUser_id: " + user_id);
+    res.end();
 });
 
 // Listen to VK callback API
