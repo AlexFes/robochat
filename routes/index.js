bce0ffb06cbd5cb444c9cab18dc1c385930ed2a0
Alex Fes <alexeiruxur@gmail.com>
2019-08-14 12:07:05 +0300

    listen callback api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const util = require('util');
+const config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+// Listen to VK callback API
+router.post('/', async (req, res) => {
+    switch (req.body.type) {
+        case 'confirmation':
+            res.status(200);
+            res.send(config.callbackApiKey);
+            break;
+
+        default:
+            console.log(util.inspect(req.body));
+            res.status(200);
+            res.send('ok');
+    }
 });
 
 module.exports = router;
