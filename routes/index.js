85d0203104636aaf37026d9283a7c24b46f04944
Alex Fes <alexeiruxur@gmail.com>
2019-08-14 16:31:36 +0300

    new auth

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,18 @@ const config = require('../config');
 
 // Get access token
 router.get('/', function(req, res) {
+    const redirect_uri = encodeURIComponent('https://oauth.vk.com/blank.html');
+
     res.redirect('https://oauth.vk.com/authorize?client_id=' + config.clientId +
-        '&redirect_uri=' + config.host +
-        'auth&scope=messages,offline&response_type=code&v=5.92');
+        '&redirect_uri=' + redirect_uri +
+        'auth&scope=messages,offline&response_type=token&v=5.92');
 });
 
-router.get('/auth', async (req, res) => {
-    res.redirect('https://oauth.vk.com/access_token?client_id=' + config.clientId +
-        '&redirect_uri=' + config.host +
-        'auth&code=' + req.query.code + '&client_secret=' + config.clientSecret);
-});
+// router.get('/auth', async (req, res) => {
+//     res.redirect('https://oauth.vk.com/access_token?client_id=' + config.clientId +
+//         '&redirect_uri=' + config.host +
+//         'auth&code=' + req.query.code + '&client_secret=' + config.clientSecret);
+// });
 
 // Listen to VK callback API
 router.post('/', async (req, res) => {
